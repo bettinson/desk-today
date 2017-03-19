01623068fa5f5fffdf9a008dae91656dd2951382
Matt Bettinson <mattbettinson@me.com>
2017-03-19 15:20:03 -0400

    Better now

--- desk-today.rb
+++ desk-today.rb
@@ -1,13 +1,15 @@
+#!/usr/bin/env ruby
 require 'date'
+require "FileUtils"
 
 date = Date.today
-desktop = "/Users/mattbettinson/Desktop"
-folder_today = "/Users/mattbettinson/Documents/#{date}"
-
+desktop = Dir.home() + "/Desktop"
+folder_today = Dir.home() +"/Documents/#{date}"
 system 'mkdir', '-p', folder_today
 
 Dir.foreach(desktop) do |file|
   if file[0] != '.'
-    system "mv #{desktop}/#{file} #{folder_today}/#{file}"
+    FileUtils.move "#{desktop}/#{file}", "#{folder_today}/#{file}"
   end
 end
+
